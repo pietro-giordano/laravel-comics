d3ca1b67d1376dae7542fb29a59c6340510875c2
Pietro Giordano <giopi87@gmail.com>
2023-03-09 16:07:08 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+const path = require('path');
+
+export default defineConfig({
+      plugins: [
+            laravel([
+                  'resources/scss/app.scss',
+                  'resources/js/app.js',
+            ]),
+      ],
+      resolve: {
+            alias: {
+                  '~resources': '/resources/',
+                  '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            }
+      }
+});
